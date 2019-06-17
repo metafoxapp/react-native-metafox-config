15a3f482dbb86a4a193a2dadc4f5a275fac67851
Nguyễn Châu Thành Thiện <thiensmail96@gmail.com>
2019-06-17 09:06:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const originConfigs = require('react-native').NativeModules.RNPhpfoxConfig.value
 
 export const Configs = Object.assign({}, originConfigs, {privateInfo: {}})
 
+export const saveUserConfig = require('react-native').NativeModules.RNPhpfoxConfig.saveUserConfig
+
 const getServerApiUrlFromServerApiUrl = (serverUrl) => {
   return (serverUrl.replace(/([/]+$)/g, '') + '/index.php/restful_api/').replace(
     'index.php/index.php', 'index.php')
