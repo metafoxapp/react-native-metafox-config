afc5e372e832682013eded4616690b5ea51273c0
Nguyễn Châu Thành Thiện <thiensmail96@gmail.com>
2019-06-17 08:53:22 +0700

    Update UserConfig for ios

--- ios/RNPhpfoxConfig.m
+++ ios/RNPhpfoxConfig.m
@@ -16,16 +16,34 @@ RCT_EXPORT_MODULE()
 
     NSString *configurationFileName = [[NSBundle mainBundle] pathForResource:@"configuration"
                                                                       ofType:@"json"];
-    
+
     NSString *googleServiceInfoFileName = [[NSBundle mainBundle] pathForResource:@"GoogleService-Info"
                                                                           ofType:@"plist"];
-    
+
     NSDictionary *googleServiceInfoDict = [NSDictionary dictionaryWithContentsOfFile:googleServiceInfoFileName];
-    
+
+    // User Config plist
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *path = [documentsDirectory stringByAppendingPathComponent:@"UserConfig.plist"];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
     //check file exists
     //retrieve file content
     NSData *infoData = [[NSData alloc] initWithContentsOfFile:configurationFileName];
-    
+
+    if (![fileManager fileExistsAtPath: path]) {
+        path = [documentsDirectory stringByAppendingPathComponent: [NSString stringWithFormat:@"UserConfig.plist"] ];
+    }
+
+    NSMutableDictionary *userConfig;
+
+    if ([fileManager fileExistsAtPath: path]) {
+        userConfig = [[NSMutableDictionary alloc] initWithContentsOfFile: path];
+    }else {
+        userConfig = [[NSMutableDictionary alloc] init];
+    }
+
     //convert JSON NSData to a usable NSDictionary
     NSError *error;
     NSMutableDictionary *result = [NSJSONSerialization JSONObjectWithData:infoData
@@ -37,11 +55,41 @@ RCT_EXPORT_MODULE()
 
     [result setValue:googleServiceInfoDict
               forKey:@"googleServiceInfo"];
-    
+
+    [result setValue:userConfig
+                forKey:@"userConfig"];
+
     [result setValue:@{}
               forKey:@"privateInfo"];
-    
+
     return @{@"values": result};
 }
+
+RCT_EXPORT_METHOD(saveUserConfig:(NSString *)key value:(NSString *)value) {
+    //Get the documents directory path
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *path = [documentsDirectory stringByAppendingPathComponent:@"UserConfig.plist"];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
+    if (![fileManager fileExistsAtPath: path]) {
+        path = [documentsDirectory stringByAppendingPathComponent: [NSString stringWithFormat:@"UserConfig.plist"] ];
+    }
+
+    NSMutableDictionary *data;
+
+    if ([fileManager fileExistsAtPath: path]) {
+        data = [[NSMutableDictionary alloc] initWithContentsOfFile: path];
+    }
+    else {
+        // If the file does not exist, create an empty dictionary
+        data = [[NSMutableDictionary alloc] init];
+    }
+
+    //To insert the data into the plist
+    [data setObject:value forKey:key];
+    [data writeToFile:path atomically:YES];
+}
+
 @end
 
